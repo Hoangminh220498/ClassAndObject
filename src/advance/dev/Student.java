947fb965708788481e77f587e01bf2ff3e72bf21
= <=>
2021-06-04 22:07:51 +0700

    first commit

---
+++ src/advance/dev/Student.java
@@ -0,0 +1,18 @@
+package advance.dev;
+
+public class Student {
+	String name;
+	int age;
+	String address;
+	int numberPhone;
+	float diemTB;
+	public Student(String name, int age, String address, int numberPhone, float diemTB) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.numberPhone = numberPhone;
+		this.diemTB = diemTB;
+	}
+
+}
